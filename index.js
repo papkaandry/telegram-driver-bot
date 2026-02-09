c9ae5fe7f6f74f9fb91a394cad9e6eff10bfbb5a
papkaandry <papkaandry@gmail.com>
2026-02-08 23:17:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,18 +8,15 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
-// Telegram webhook
 app.post('/', async (req, res) => {
   await handleUpdate(req.body);
   res.send('OK');
 });
 
-// Health check
 app.get('/', (req, res) => {
   res.send('Bot is running');
 });
 
-// Start server + migrate DB
 app.listen(PORT, async () => {
   await migrate();
   console.log('🤖 Bot started');
