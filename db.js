81df76e4822ce2f0a906d03bdf4cb61515c24234
papkaandry <papkaandry@gmail.com>
2026-02-08 22:07:27 -0800

    Refactor PostgreSQL pool import syntax

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
-import pg from 'pg';
+import pkg from 'pg';
+const { Pool } = pkg;
 
-export const pool = new pg.Pool({
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
+});
+
+pool.on('connect', () => {
+  console.log('🟢 Connected to PostgreSQL');
 });
