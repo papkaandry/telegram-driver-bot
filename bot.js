9b921a61018f2b2b01f7015c690afda83d21d6db
papkaandry <papkaandry@gmail.com>
2026-02-08 22:08:15 -0800

    Refactor bot.js to use token and ADMIN_ID constants

--- bot.js
+++ bot.js
@@ -1,5 +1,12 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-export const bot = new TelegramBot(process.env.BOT_TOKEN, {
+const token = process.env.BOT_TOKEN;
+export const ADMIN_ID = Number(process.env.ADMIN_ID);
+
+export const bot = new TelegramBot(token, {
   polling: true
+});
+
+bot.on('message', (msg) => {
+  console.log(`📩 ${msg.from.id}: ${msg.text}`);
 });
