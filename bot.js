3aad6d727f5cb08c5a47e04c4681bf7706b543c1
papkaandry <papkaandry@gmail.com>
2026-02-15 22:43:07 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import { pool } from './db.js';
 import { sendMail } from './mail.js';
 
 const ADMIN_ID = "427968134";
-const GROUP_CHAT_ID = "-511165308";
+const GROUP_CHAT_ID = "-5111653088";
 
 
 export function setupBot(bot) {
